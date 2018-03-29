9d1752f6aecd65724ea587119a7f7517c18ce135
skyrior <12475669+skyrior@users.noreply.github.com>
2018-03-29 13:33:05 -0300

    Finalização

--- src/Autor.js
+++ src/Autor.js
@@ -6,16 +6,13 @@ import TratadorErros from './TratadorErros';
 
 class FormularioAutor extends Component {
 
-	constructor() {  
+  constructor() {
     super();
     this.state = { nome: '', email: '', senha: '' };
     this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-	}
+  }
 
-	enviaForm(evento){
+  enviaForm(evento) {
     evento.preventDefault();
     console.log('nome: ' + this.state.nome + ' email: ' + this.state.email + ' senha: ' + this.state.senha);
     $.ajax({
@@ -23,113 +20,105 @@ class FormularioAutor extends Component {
       contentType: 'application/json',
       dataType: 'json',
       type: 'post',
-      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
-      success: function(response){
-				PubSub.publish('lista-autores', response);
-				this.setState({nome:'', email:'', senha: ''});
+      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
+      success: function (response) {
+        PubSub.publish('lista-autores', response);
+        this.setState({ nome: '', email: '', senha: '' });
       }.bind(this),
-      error: function(response){
-				if(response.status === 400) {
-					new TratadorErros().publicaErros(response.responseJSON);
-				}
-			},
-			beforeSend: function(){
-				new TratadorErros().limpaErros();
-			}
+      error: function (response) {
+        if (response.status === 400) {
+          new TratadorErros().publicaErros(response.responseJSON);
+        }
+      },
+      beforeSend: function () {
+        new TratadorErros().limpaErros();
+      }
     })
   }
-
-  setNome(evento){
-    this.setState({nome: evento.target.value});
+  
+  salvaAlteracao(nomeInput, evento) {
+    this.setState({ [nomeInput]: evento.target.value });
   }
 
-  setEmail(evento){
-    this.setState({email: evento.target.value});
+  render() {
+    return (
+      <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+          <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.salvaAlteracao.bind(this, 'nome')} label="Nome" />
+          <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.salvaAlteracao.bind(this, 'email')} label="Email" />
+          <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.salvaAlteracao.bind(this, 'senha')} label="Senha" />
+          <div className="pure-control-group">
+            <label></label>
+            <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+          </div>
+        </form>
+      </div>
+    )
   }
-
-  setSenha(evento){
-    this.setState({senha: evento.target.value});
-  }
-	
-	render() {
-		return (
-			<div className="pure-form pure-form-aligned">
-				<form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-					<InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
-					<InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
-					<InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
-					<div className="pure-control-group">
-						<label></label>
-						<button type="submit" className="pure-button pure-button-primary">Gravar</button>
-					</div>
-				</form>
-			</div>
-		)
-	}
 }
 
 class TabelaAutor extends Component {
-	
-	render() {
-		return (
-				<div>
-					<table className="pure-table">
-						<thead>
-							<tr>
-								<th>Nome</th>
-								<th>email</th>
-							</tr>
-						</thead>
-						<tbody>
-							{
-								this.props.lista.map(function (autor) {
-									return (
-										<tr key={autor.id}>
-											<td>{autor.nome}</td>
-											<td>{autor.email}</td>
-										</tr>
-									);
-								})
-							}
-						</tbody>
-					</table>
-				</div>
-		)
-	}
+
+  render() {
+    return (
+      <div>
+        <table className="pure-table">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.lista.map(function (autor) {
+                return (
+                  <tr key={autor.id}>
+                    <td>{autor.nome}</td>
+                    <td>{autor.email}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default class AutorBox extends Component {
-	
-	constructor() {  
-    super();
-		this.state = { lista : [] };
-	}
 
-	componentDidMount(){
+  constructor() {
+    super();
+    this.state = { lista: [] };
+  }
+
+  componentDidMount() {
     $.ajax({
-      url:'http://cdc-react.herokuapp.com/api/autores',
+      url: 'http://cdc-react.herokuapp.com/api/autores',
       dataType: 'json',
-      success: function(response){
-        this.setState({lista: response});
+      success: function (response) {
+        this.setState({ lista: response });
       }.bind(this)
-		});
-		
-		PubSub.subscribe('lista-autores', function(topico, response){
-			this.setState({lista: response});
-		}.bind(this));
-	}
-	
-	render() {
-		return (
-			<div>
-				<div className="header">
-					<h1>Lista de Autores</h1>
-				</div>
-				<div className="content" id="content">
-					<FormularioAutor/>
-					<TabelaAutor lista={this.state.lista}/>
-				</div>
-			</div>
-		)
-	}
+    });
+
+    PubSub.subscribe('lista-autores', function (topico, response) {
+      this.setState({ lista: response });
+    }.bind(this));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <h1>Lista de Autores</h1>
+        </div>
+        <div className="content" id="content">
+          <FormularioAutor />
+          <TabelaAutor lista={this.state.lista} />
+        </div>
+      </div>
+    )
+  }
 }
